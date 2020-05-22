86031d6fb8293cd97e48053719116a5ed1d2ff21
jerillo <jesnineerillo@berkeley.edu>
2020-05-21 22:44:37 -0700

    installed packages and setup mongoose

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const passport = require('passport');
 
 // Require routes
 const indexRoutes = require('./routes/index');
+
+mongoose.connect('mongodb://localhost:27017/internships', {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
 // View engine setup
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +19,9 @@ app.set('view engine', 'ejs');
 // Routes
 app.use('/', indexRoutes);
 
-app.listen(3000, () => {
+// Passport config here
+
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is listening on port 3000');
 });
