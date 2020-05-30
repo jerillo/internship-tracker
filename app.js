6717d2e473ae3a660c07dd86466e3097b73140c2
jerillo <jesnineerillo@berkeley.edu>
2020-05-29 22:31:16 -0700

    Removed ejs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const passport = require('passport');
 
@@ -9,15 +8,13 @@ const passport = require('passport');
 const indexRoutes = require('./routes/index');
 
 mongoose.connect('mongodb://localhost:27017/internships', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(methodOverride('_method'));
-
-// View engine setup
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', indexRoutes);
@@ -25,5 +22,5 @@ app.use('/', indexRoutes);
 // Passport config here
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000');
 });
