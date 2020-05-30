39a02e355a6ebb928c4486ae573bb2a7d1001ad4
jerillo <jesnineerillo@berkeley.edu>
2020-05-29 21:35:17 -0700

    Reformatted code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -8,9 +8,12 @@ const passport = require('passport');
 // Require routes
 const indexRoutes = require('./routes/index');
 
-mongoose.connect('mongodb://localhost:27017/internships', {useNewUrlParser: true, useUnifiedTopology: true});
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
+mongoose.connect('mongodb://localhost:27017/internships', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // View engine setup
 app.use(express.static(__dirname + '/public'));
@@ -21,7 +24,6 @@ app.use('/', indexRoutes);
 
 // Passport config here
 
-
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is listening on port 3000');
+	console.log('Server is listening on port 3000');
 });
