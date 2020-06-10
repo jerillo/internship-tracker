a84f6c3d7e2c325defca3ccacd1c3115a2e7aea4
jerillo <jesnineerillo@berkeley.edu>
2020-06-10 00:28:49 -0700

    implemented oauth with google using passport

--- app.js
+++ app.js
@@ -1,40 +1,86 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('./models/user');
 require('dotenv').config();
 
-// Require routes
-const indexRoutes = require('./routes/index');
-
 mongoose.connect('mongodb://localhost:27017/internships', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}));
-
-// Routes
-app.use('/', indexRoutes);
-
-// Passport Configuration
+app.use(express.json());
 app.use(
-    require('express-session')({
-        secret: process.env.SECRET,
-        resave: false,
-        saveUninitialized: false
-    })
+	express.urlencoded({
+		extended: true
+	})
 );
+
+// Defining routes
+app.use('/', require('./routes/index'));
+
+// Google Strategy
 app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, cb) => {
+	cb(null, user.id);
+});
+passport.deserializeUser((id, cb) => {
+	User.findById(id).then((user) => {
+		cb(null, user);
+	});
+});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, cb) => {
+			// console.log(JSON.stringify(profile, null, 2));
+			let user = { ...profile };
+			console.log(user);
+			return cb(null, profile);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: [ 'profile', 'email' ]
+	})
+);
+
+app.get(
+	'/auth/google/callback',
+	passport.authenticate('google'),
+	(req, res) => {
+		res.redirect('/');
+	}
+);
+
+app.get('/user', (req, res) => {
+	console.log('getting user data!');
+	res.send(user);
+});
+
+app.get('/auth/logout', (req, res) => {
+	console.log('logging out!');
+	user = {};
+	res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+	if (req.session.user !== undefined) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+}
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is listening on port 3000');
+	console.log('Server is listening on port 3000');
 });
--- models/user.js
+++ models/user.js
@@ -4,14 +4,15 @@ const passportLocalMongoose = require('passport-local-mongoose');
 // TODO: Integrate Google OAuth 2.0 via Passport.js
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    internships: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Internship'
-        }
-    ]
+	username: String,
+	password: String,
+	email: String,
+	internships: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Internship'
+		}
+	]
 });
 
 UserSchema.plugin(passportLocalMongoose);
